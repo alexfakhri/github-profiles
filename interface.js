abf63a3fe09550503221d9bac6202a8d3c2f75fa
Alex Fakhri <alexfakhri@me.com>
2016-09-22 11:36:44 +0100

    Added handlebars template to html

--- interface.js
+++ interface.js
@@ -1,6 +1,8 @@
 $('#fetch-profile').click(function(event){
   event.preventDefault();
-  var profile = 'https://api.github.com/users/alexfakhri?client_id=?270ae81c14074e2c3045&client_secret=2ec458a20ed0a813fd47436f859b1fc5e260d78e';
+  var username = $('#username').val()
+  var template = $('#profileTemplate').html()
+  var profile = 'https://api.github.com/users/' + username +'?client_id=?270ae81c14074e2c3045&client_secret=2ec458a20ed0a813fd47436f859b1fc5e260d78e';
   $.get(profile, function(user){
   console.log(user);
   });
