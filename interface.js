02269d4c8917eb12c764b33381eace7296dfcf3e
Alex Fakhri <alexfakhri@me.com>
2016-09-22 14:53:49 +0100

    Code cleaned up and started to add styling

--- interface.js
+++ interface.js
@@ -5,9 +5,6 @@ $('#fetch-profile').click(function(event){
   var template = Handlebars.compile(html)
   var profile = 'https://api.github.com/users/' + username +'?client_id=?270ae81c14074e2c3045&client_secret=2ec458a20ed0a813fd47436f859b1fc5e260d78e';
   $.get(profile, function(user){
-  console.log(user);
-  console.log(template(user));
-  $('#profile').append(template(user));
+    $('#profile').append(template(user));
   });
-
 });
