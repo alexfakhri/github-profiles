117f7d81d1b1089893123c196ce4d117e292d03c
Alex Fakhri <alexfakhri@me.com>
2016-09-22 13:05:34 +0100

    Now pulls in profile and using handlebars template renders it on page

--- interface.js
+++ interface.js
@@ -1,10 +1,13 @@
 $('#fetch-profile').click(function(event){
   event.preventDefault();
   var username = $('#username').val()
-  var template = $('#profileTemplate').html()
+  var html = $('#profileTemplate').html()
+  var template = Handlebars.compile(html)
   var profile = 'https://api.github.com/users/' + username +'?client_id=?270ae81c14074e2c3045&client_secret=2ec458a20ed0a813fd47436f859b1fc5e260d78e';
   $.get(profile, function(user){
   console.log(user);
+  console.log(template(user));
+  $('#profile').append(template(user));
   });
 
 });
